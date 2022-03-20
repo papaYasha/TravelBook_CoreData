26397f7a888c2a62cb1256c4b03b9b374589b26b
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-20 10:26:42 +0300

    Init project

---
+++ TravelBook_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelBook_CoreData
+//
+//  Created by Macbook on 20.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
