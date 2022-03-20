bec8dadc178a20c75e029c4f02164efe48762971
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-20 12:03:31 +0300

    Implement MapKit, UserLocation, Pins on map

--- TravelBook_CoreData/ViewController.swift
+++ TravelBook_CoreData/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        config()
     }
-
-
+    
+    private func config() {
+        configMapView()
+        configLocationManager()
+        configGestureRec()
+    }
+    
+    private func configMapView() {
+        mapView.delegate = self
+    }
+    
+    private func configLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func configGestureRec() {
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation))
+        gestureRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc private func chooseLocation(rec: UILongPressGestureRecognizer) {
+        if rec.state == .began {
+            let touchedPoints = rec.location(in: self.mapView)
+            let touchedCoordinates = self.mapView.convert(touchedPoints, toCoordinateFrom: self.mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinates
+            annotation.title = "New annotation"
+            annotation.subtitle = "Travel book"
+            self.mapView.addAnnotation(annotation)
+        }
+    }
 }
+
+extension ViewController: MKMapViewDelegate {
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
 
